c07dc04d7423704e3ae74fb06414e109151cf96e
Nicolas Castellanos <nicolas.castellanos012@gmail.com>
2023-10-04 22:00:29 -0500

    funciones asincronas

--- listaTareas.js
+++ listaTareas.js
@@ -1,35 +1,50 @@
 //declaro el readline que me ayudara mas adelante para que el usuario pueda interactuar con el programa
 const readline = require('readline-sync');
 //declare un array vacio ya que este se ira llenando o vaciando dependiendo de la accion que se decida realizar
-const listaDeTrareas = [];
+let listaDeTrareas = [];
 //agregue la funcion de agregar una tarea con parametros de indicador y descripcion. para luego mandar la info al array vacio
 function agregarTarea(indicador, descripcion) {
-    listaDeTrareas.push({
-        indicador, descripcion, completada: false
-    });
-    console.log('tarea agregada: ' + indicador + ' - ' + descripcion);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            listaDeTrareas.push({
+            indicador, descripcion, completada: false});
+            console.log('tarea agregada: ' + indicador + ' - ' + descripcion);
+            resolve();
+        }, 3000);
+       
+    }); 
 };
 //agregue la funcion de eliminar tarea 
 function eliminarTarea(indicador) {
-    //use el find para encontrar en el array una tarea que cumpla con la condicion 
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
     const findTarea = listaDeTrareas.findIndex(tarea => tarea.indicador === indicador);
     if (findTarea !== -1) {
         listaDeTrareas.splice(findTarea, 1);
-        console.log('Tarea eliminada ' + indicador);
+       resolve(console.log('Tarea eliminada ' + indicador));
     } else {
-        console.log('No se encontro la tarea con indicador: ' + indicador);
+        reject(console.log('No se encontro la tarea con indicador: ' + indicador));
     }
+        }, 3000);
+    });
+    //use el find para encontrar en el array una tarea que cumpla con la condicion 
+   
 };
 //agregue la funcion de completar una tarea
 function tareaCompletada(indicador) {
-    //aca busco la tarea que deseo completar
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
     const tarea = listaDeTrareas.find(tarea => tarea.indicador === indicador);
     if (tarea) {
         tarea.completada = true;
-        console.log('La tarea ' + indicador + ' a sido completada: ');
+       resolve(console.log('La tarea ' + indicador + ' a sido completada: '));
     } else {
-        console.log('La tarea no se ha encontrado: ' + indicador);
-    }
+        reject(console.log('La tarea no se ha encontrado: ' + indicador));
+    } 
+        }, 3000);
+    });
+    //aca busco la tarea que deseo completar
+   
 };
 //agregue un funcion que me permite ver las tareas que he agregado en el array
 function mostrarTareas() {
@@ -40,43 +55,55 @@ function mostrarTareas() {
     });
 };
 //agregue la funcion que me permite ver un menu y elejir una opcion
-function menu() {
+async function menu() {
     console.log('Menu de opciones');
     //agregue un while para el menu se puede repetir mas de una vez, asi no se tenga que iniciar el programa cada vez que termine una accion
-    while (true) {
-        console.log('Opciones');
-        console.log('1. Agregar tarea');
-        console.log('2. Eliminar tarea');
-        console.log('3. completar tarea');
-        console.log('4. Mostrar tareas');
-        console.log('5. Salir');
-        //aca permito que el usuario elija una de las opciones y pueda realizar una accion
-        const opcion = readline.question('Elije una opcion: ');
-        //utilice un switch para la serie de opciones que puse y por cada opcion ocurre algo diferente 
-        switch (opcion) {
-            case '1':
-                const indicador = readline.question('Indicador de la nueva tarea: ');
-                const descripcion = readline.question('Descripcion de la nueva tarea: ');
-                agregarTarea(indicador, descripcion);
-                break;
-            case '2':
-                const indicadorAElinimar = readline.question('Indicador de la tarea que se desea eliminar: ');
-                eliminarTarea(indicadorAElinimar);
-                break;
-            case '3':
-                const tareaCompletar = readline.question('Indicador de la tarea que se desea completar: ');
-                tareaCompletada(tareaCompletar);
-                break;
-            case '4':
-                mostrarTareas();
-                break;
-            case '5':
-                console.log('Adios :,c');
-                return;
-            default:
-                console.log('Esa opcion no esta en el menu prueba nuevamente');
+        while (true) {
+            console.log('Opciones');
+            console.log('1. Agregar tarea');
+            console.log('2. Eliminar tarea');
+            console.log('3. completar tarea');
+            console.log('4. Mostrar tareas');
+            console.log('5. Salir');
+            //aca permito que el usuario elija una de las opciones y pueda realizar una accion
+            const opcion = await readline.question('Elije una opcion: ');
+            //utilice un switch para la serie de opciones que puse y por cada opcion ocurre algo diferente 
+            switch (opcion) {
+                case '1':
+                    try{
+                    const indicador = await readline.question('Indicador de la nueva tarea: ');
+                    const descripcion = await readline.question('Descripcion de la nueva tarea: ');
+                        await agregarTarea(indicador, descripcion);
+                    }catch(error) {
+                     console.log('error ' +error);   
+                    }
+                    break;
+                case '2':
+                    try {
+                        const indicadorAElinimar = await readline.question('Indicador de la tarea que se desea eliminar: ');
+                        await eliminarTarea(indicadorAElinimar);
+                    } catch (error) {
+                        console.log('error '+error);
+                    }
+                    break;
+                case '3':
+                    try {
+                        const tareaCompletar = await readline.question('Indicador de la tarea que se desea completar: ');
+                        await tareaCompletada(tareaCompletar);
+                    } catch (error) {
+                        console.log('error '+error);
+                    }
+                    break;
+                case '4':
+                   await mostrarTareas();
+                    break;
+                case '5':
+                    await console.log('Adios :,c');
+                    return;
+                default:
+                    await console.log('Esa opcion no esta en el menu prueba nuevamente');
+            }
         }
-    }
-}
+    } 
 //llame la funcion menu para poder veer el menu 
 menu();
